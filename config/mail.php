69b4f153dbdee1cbfeb1cb388e4cda40241f3b62
Dimar <cocadimar@gmail.com>
2021-02-10 23:43:16 -0400

    MODIFICACIONES

--- config/mail.php
+++ config/mail.php
@@ -84,8 +84,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'cocadimar@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Dimar'),
     ],
 
     /*
