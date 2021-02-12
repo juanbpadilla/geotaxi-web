191b3e3e99a0d1576630ee53e5b31dd30e24743b
Juan <juanbpadillac@gmail.com>
2021-02-12 01:25:57 -0400

    google change 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="google-site-verification" content="aXQNmDHwOes9xF79r0il4H-tP5vx5qNzq7groNWRN9U" />
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
