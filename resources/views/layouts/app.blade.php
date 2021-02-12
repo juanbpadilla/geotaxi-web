76029606bcba44ef5745758099072d2e4c3d4608
Juan <juanbpadillac@gmail.com>
2021-02-12 04:42:47 -0400

    unchange latitude 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,7 +66,7 @@
     });
     infoWindow = new google.maps.InfoWindow();
     const locationButton = document.createElement("button");
-    locationButton.textContent = "Pan to Current Location";
+    locationButton.textContent = "Encontrar mi ubicación actual";
     locationButton.classList.add("custom-map-control-button");
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(
       locationButton
@@ -81,7 +81,7 @@
               lng: position.coords.longitude,
             };
             infoWindow.setPosition(pos);
-            infoWindow.setContent("Location found.");
+            infoWindow.setContent("Usted esta aquí.");
             infoWindow.open(map);
             map.setCenter(pos);
           },
