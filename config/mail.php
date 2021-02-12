70ece72b3b6e92fb9da0c8de540ddda690f2c43a
Juan <juanbpadillac@gmail.com>
2021-02-12 03:33:02 -0400

    arreglando eventos 1

--- config/mail.php
+++ config/mail.php
@@ -84,7 +84,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'cocadimar@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'padillacjuanb@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Geotaxi'),
     ],
 
