9d99ce9de10a70ce78c7672a8d6c9d87c91a115e
Juan <juanbpadillac@gmail.com>
2021-02-12 01:30:26 -0400

    home change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -58,7 +58,7 @@
                     <div id="map"></div>
                     <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
                     <script
-                          src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDY0rOo5NaiHngwJ6VIjpfah7RCFffHEcs&libraries=&v=weekly"
+                          src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDY0rOo5NaiHngwJ6VIjpfah7RCFffHEcs&callback=initMap&libraries=&v=weekly"
                           async
                     ></script>
                   </div>
